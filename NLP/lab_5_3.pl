c1b839b074dd8f2576f0d251b1f902c2d2fb05db
Vlad-Doru Ion <vlad.doru@gmail.com>
2014-11-06 16:16:47 +0200

    Finished 3rd exercise

--- NLP/lab_5_3.pl
+++ NLP/lab_5_3.pl
@@ -2,33 +2,35 @@
 % Problema 3
 
 % Regula pentru convenienta.
-parse(X) :- parse(X, [s], []).
+parse(X, TP) :- parse(X, [s], [], TP).
 
 % Vom folosi o stiva de numere
 
 % Avem repl_red(sir_ramas, stiva, rezultat).
-parse(S, Rezultat, Numere) :- depl_red(S, [], Rezultat, Numere).
+parse(S, Rezultat, Numere, TP) :- depl_red(S, [], Rezultat, Numere, [], TP).
 
 % Dorim sa nu mai existe nimic si sa ajungem la stiva dorita.
-depl_red([], Rezultat, Rezultat, _).
-depl_red(S, Stiva, Rezultat, Numere) :- deplasare(Stiva, S, StivaNoua, S1),
-                                        reducere(StivaNoua, StivaRedusa, Numere, NumereReduse),
-                                        depl_red(S1, StivaRedusa, Rezultat, NumereReduse).
+depl_red([], Rezultat, Rezultat, _, [PT | _], PT).
+depl_red(S, Stiva, Rezultat, Numere, SPT, PT) :-
+  deplasare(Stiva, S, StivaNoua, S1),
+  reducere(StivaNoua, StivaRedusa, Numere, NumereReduse, SPT, RPT),
+  depl_red(S1, StivaRedusa, Rezultat, NumereReduse, RPT, PT).
 
 deplasare(Stiva, [H | Y], [H | Stiva], Y).
 
 % Nu reducem nimic
-reducere(Stiva, Stiva, Numere, Numere).
+reducere(Stiva, Stiva, Numere, Numere, SPT, SPT).
 % Reducem daca exista regula, apoi incercam sa aplicam o reducere succesiva.
-reducere(Stiva, StivaRedusa, Numere, NumereReduse) :- iregula(Stiva, Stiva1, Numere, Numere1),
-                                                      reducere(Stiva1, StivaRedusa, Numere1, NumereReduse).
+reducere(Stiva, StivaRedusa, Numere, NumereReduse, SPT, PT) :-
+  iregula(Stiva, Stiva1, Numere, Numere1, SPT, SPT1),
+  reducere(Stiva1, StivaRedusa, Numere1, NumereReduse, SPT1, PT).
 % NP si VP se acorda in verb in timp ce s nu are numar.
-iregula([vp, np | X], [s | X], [Numar, Numar | Y], Y).
-iregula([np, vp | X], [vp | X], [_, Numar | Y], [Numar | Y]).
-iregula([v | X], [vp | X], [Numar | Y], [Numar | Y]).
-iregula([n | X], [np | X], [Numar | Y], [Numar | Y]).
+iregula([vp, np | X], [s | X], [Numar, Numar | Y], Y, [S, T | SPT], [[s, T, S] | SPT]).
+iregula([np, vp | X], [vp | X], [_, Numar | Y], [Numar | Y], [S, T | SPT], [[vp, T, S] | SPT]).
+iregula([v | X], [vp | X], [Numar | Y], [Numar | Y], [S | SPT], [[vp, S] | SPT]).
+iregula([n | X], [np | X], [Numar | Y], [Numar | Y], [S | SPT], [[np, S] | SPT]).
 
-iregula([Cuvant | X], [Cat | X], Y, [Numar | Y]) :- cuvant(Cat, Numar, Cuvant).
+iregula([Cuvant | X], [Cat | X], Y, [Numar | Y], SPT, [[Cat, [Cuvant]] | SPT]) :- cuvant(Cat, Numar, Cuvant).
 
 cuvant(n, singular, publicul).
 cuvant(n, plural, artistii).
