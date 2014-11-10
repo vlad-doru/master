e815152418efd69772a9b43b194bffe36d731b7f
Vlad-Doru Ion <vlad.doru@gmail.com>
2014-11-10 12:38:15 +0200

    Lint prob 1

--- NLP/lab_5_1.pl
+++ NLP/lab_5_1.pl
@@ -3,14 +3,23 @@
 
 % Problema 1.
 
+% Regula pentru a putea parsa mai usor fraze si a genera arborele.
+parse(X, Arbore) :- parse(s, _, X, [], Arbore).
 % Atunci cand parsam un constituent trebuie sa fim siguri ca numarul
 % acestuia este cel corect.
-parse(C, Numar, [Cuvant | S], S, [C, [Cuvant]]) :-  cuvant(C, Numar, Cuvant).
+parse(Simbol, Numar, [Cuvant | S], S, [Simbol, [Cuvant]]) :-
+    cuvant(Simbol, Numar, Cuvant).
 % Atunci cand aplicam o regula, fiecare din constituentii ce vor fi adaugati
 % vor genera la randul lor un numar. Este important sa ii acordam pe fiecare dintre
 % acesteia la randul lor in numar.
-parse(C, Numar, S1, S, [C, Y]) :- regula(C, Numar, Numere, Cs), parse_lista(Cs, Numere, S1, S, Y).
-parse_lista([C|Cs], [Numar | N], S1, S, [Y1 | Y2]) :- parse(C, Numar, S1, S2, Y1), parse_lista(Cs, N, S2, S, Y2).
+parse(Simbol, Numar, Sir, SirFinal, [Simbol, Arbore]) :-
+    regula(Simbol, Numar, Numere, Simboluri),
+    parse_lista(Simboluri, Numere, Sir, SirFinal, Arbore).
+
+parse_lista([Simbol | Simboluri], [Numar | Numere], Sir, SirFinal, [Arbore1 | Arbore2]) :-
+    parse(Simbol, Numar, Sir, SirIntermediar, Arbore1),
+    parse_lista(Simboluri, Numere, SirIntermediar, SirFinal, Arbore2).
+% Regula de oprire atunci cand parsam lista vida.
 parse_lista([], [], S, S, []).
 
 regula(s, _, [Numar, Numar], [np, vp]).
@@ -23,6 +32,4 @@ cuvant(n, plural, artistii).
 cuvant(v, singular, iubeste).
 cuvant(v, plural, iubesc).
 
-parse(X, ParseTree) :- parse(s, _, X, [], ParseTree).
-
-generate_all(L) :- findall(X, parse(s, _, X, []), L).
+generate_all(L) :- findall(X, parse(X, _), L).
