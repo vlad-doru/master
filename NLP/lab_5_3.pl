179f82cffb5531c88ebff306e6c26f6f96459e11
Vlad-Doru Ion <vlad.doru@gmail.com>
2014-11-10 13:50:17 +0200

    Neat 3

--- NLP/lab_5_3.pl
+++ NLP/lab_5_3.pl
@@ -4,35 +4,37 @@
 % Problema 3.
 
 % Regula pentru parsarea mai usoara.
-parse(X, TP) :- parse(X, [s], [], TP).
+parse(X, Arbore) :- parse(X, [s], [], Arbore).
 
 % Vom folosi o stiva de numere
+parse(Sir, Rezultat, Numere, Arbore) :-
+    depl_red(Sir, [], Rezultat, Numere, [], Arbore).
+% depl_red(Sir, StivaInitiala, Rezultat, Numere, StivaNumere, Arbore).
 
-% Avem repl_red(sir_ramas, stiva, rezultat).
-parse(S, Rezultat, Numere, TP) :- depl_red(S, [], Rezultat, Numere, [], TP).
+% Dorim sa parsam complet sirul si sa ajungem la rezultatul dorit.
+depl_red([], Rezultat, Rezultat, _, [Arbore], Arbore).
+% Regula cand nu am parsat complet intreg sirul.
+depl_red(Sir, Stiva, Rezultat, Numere, Arbore, ArboreFinal) :-
+  deplasare(Stiva, Sir, StivaNoua, SirDeplasat),
+  reducere(StivaNoua, StivaRedusa, Numere, NumereReduse, Arbore, ArboreInter),
+  depl_red(SirDeplasat, StivaRedusa, Rezultat, NumereReduse, ArboreInter, ArboreFinal).
 
-% Dorim sa nu mai existe nimic si sa ajungem la stiva dorita.
-depl_red([], Rezultat, Rezultat, _, [PT | _], PT).
-depl_red(S, Stiva, Rezultat, Numere, SPT, PT) :-
-  deplasare(Stiva, S, StivaNoua, S1),
-  reducere(StivaNoua, StivaRedusa, Numere, NumereReduse, SPT, RPT),
-  depl_red(S1, StivaRedusa, Rezultat, NumereReduse, RPT, PT).
-
-deplasare(Stiva, [H | Y], [H | Stiva], Y).
+deplasare(Stiva, [Termen | SirDeplasat], [Termen | Stiva], SirDeplasat).
 
 % Nu reducem nimic
-reducere(Stiva, Stiva, Numere, Numere, SPT, SPT).
+reducere(Stiva, Stiva, Numere, Numere, Arbore, Arbore).
 % Reducem daca exista regula, apoi incercam sa aplicam o reducere succesiva.
-reducere(Stiva, StivaRedusa, Numere, NumereReduse, SPT, PT) :-
-  iregula(Stiva, Stiva1, Numere, Numere1, SPT, SPT1),
-  reducere(Stiva1, StivaRedusa, Numere1, NumereReduse, SPT1, PT).
+reducere(Stiva, StivaRedusa, Numere, NumereReduse, Arbore, ArboreFinal) :-
+  iregula(Stiva, Stiva1, Numere, Numere1, Arbore, ArboreInter),
+  reducere(Stiva1, StivaRedusa, Numere1, NumereReduse, ArboreInter, ArboreFinal).
 % NP si VP se acorda in verb in timp ce s nu are numar.
-iregula([vp, np | X], [s | X], [Numar, Numar | Y], Y, [S, T | SPT], [[s, T, S] | SPT]).
+iregula([vp, np | X], [s | X], [Numar, Numar | Y], [_ | Y], [S, T | SPT], [[s, T, S] | SPT]).
 iregula([np, vp | X], [vp | X], [_, Numar | Y], [Numar | Y], [S, T | SPT], [[vp, T, S] | SPT]).
 iregula([v | X], [vp | X], [Numar | Y], [Numar | Y], [S | SPT], [[vp, S] | SPT]).
 iregula([n | X], [np | X], [Numar | Y], [Numar | Y], [S | SPT], [[np, S] | SPT]).
 
-iregula([Cuvant | X], [Cat | X], Y, [Numar | Y], SPT, [[Cat, [Cuvant]] | SPT]) :- cuvant(Cat, Numar, Cuvant).
+iregula([Cuvant | X], [Cat | X], Y, [Numar | Y], Arbore, [[Cat, [Cuvant]] | Arbore]) :-
+    cuvant(Cat, Numar, Cuvant).
 
 cuvant(n, singular, publicul).
 cuvant(n, plural, artistii).
