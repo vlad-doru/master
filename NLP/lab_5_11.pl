3735dd228409ec38b9cc6a345aa5d422af1a1a1c
Vlad-Doru Ion <vlad.doru@gmail.com>
2014-11-10 15:07:10 +0200

    Problema 11

--- NLP/lab_5_11.pl
+++ NLP/lab_5_11.pl
@@ -1,40 +1,55 @@
-% Ion Vlad-Doru
+% Vlad-Doru Ion
 % Sisteme distribuite
 
 % Problema 11
 
-parse(S) :- parse(s, S, []).
+parse(Sir) :- parse(s, Sir, []).
 
-parse(C, [X | S1], S) :- cuvant(W, X),
-                   call(W, C, S1, S).
+parse(SimbolFinal, [Cuvant | SirRest], SirFinal) :-
+    cuvant(Categorie, Cuvant),
+    call(Categorie, SimbolFinal, SirRest, SirFinal).
 
 % Regulile gramaticii + reguli oprire
-s(s, X, X).
+s(s, Sir, Sir).
 
-np(np, X, X).
+np(np, Sir, Sir).
 % NP -> NP Conj NP
-np(C, S1, S) :- parse(conj, S1, S2), parse(np, S2, S3), np(C, S3, S).
-% S -> NP VP
-np(C, S1, S) :- parse(vp, S1, S2), s(C, S2, S).
+np(C, Sir, SirFinal) :-
+    parse(conj, Sir, Sir1),
+    parse(np, Sir1, Sir2),
+    np(C, Sir2, SirFinal).
+% SirFinal -> NP VP
+np(C, Sir, SirFinal) :-
+    parse(vp, Sir, Sir1),
+    s(C, Sir1, SirFinal).
 
-det(det, X, X).
+det(det, Sir, Sir).
 % NP -> Det N
-det(C, S1, S) :- parse(n, S1, S2), np(C, S2, S).
+det(C, Sir, SirFinal) :-
+    parse(n, Sir, Sir1),
+    np(C, Sir1, SirFinal).
 
-v(v, X, X).
+v(v, Sir, Sir).
 % VP -> V NP
-v(C, S1, S) :- parse(np, S1, S2), vp(C, S2, S).
+v(C, Sir, SirFinal) :-
+    parse(np, Sir, Sir1),
+    vp(C, Sir1, SirFinal).
 % VP -> V NP PP
-v(C, S1, S) :- parse(np, S1, S2), parse(pp, S2, S3), vp(C, S3, S).
+v(C, Sir, SirFinal) :-
+    parse(np, Sir, Sir1),
+    parse(pp, Sir1, Sir2),
+    vp(C, Sir2, SirFinal).
 
-p(p, X, X).
+p(p, Sir, Sir).
 % PP -> P NP
-p(C, S1, S) :- parse(np, S1, S2), pp(C, S2, S).
+p(C, Sir, SirFinal) :-
+    parse(np, Sir, Sir1),
+    pp(C, Sir1, SirFinal).
 
-vp(vp, X, X).
-n(n, X, X).
-pp(pp, X, X).
-conj(conj, X, X).
+vp(vp, Sir, Sir).
+n(n, Sir, Sir).
+pp(pp, Sir, Sir).
+conj(conj, Sir, Sir).
 
 % Lexicon
 cuvant(det, the).
