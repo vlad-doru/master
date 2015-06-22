b9c308d035e388f27714d89ff3b91f99ac42a8af
Vlad-Doru Ion <vlad.doru@gmail.com>
2015-06-22 13:07:14 +0300

    C4 sincron.

---
+++ ICLP/c4_sincron.hs
@@ -0,0 +1,49 @@
+-- Comunicare sincrona --
+-- Un thread – reader- citeste un fisier text linie cu linie
+--
+-- Liniile sunt trimise, pe rand, 
+-- unui al doilea thread – writer- care le afiseaza 
+-- in ordinea trimisa si le numara.
+--
+-- La sfarsit,  thread-ul writer trimite thread-ului reader 
+-- numarul de linii si acesta il afiseaza,
+
+import Control.Monad
+import Control.Concurrent
+import System.Environment
+import System.IO
+
+data Message = Line String | Stop
+
+master :: String -> (Chan Message) -> (MVar Integer) -> IO ()
+master file c m = do
+  text <- readFile file
+  let contents = lines text
+  putMessage contents
+  n <- takeMVar m
+  putStrLn $ "Count: " ++ (show n)
+  return ()
+  where 
+    putMessage contents 
+      | contents == [] = do
+          writeChan c Stop
+          print "Finished streaming."
+      | otherwise = do
+          writeChan c (Line (head contents))
+          putMessage $ tail contents
+
+worker :: (Chan Message) -> (MVar Integer) -> IO ()
+worker c m = loop 0 where
+  loop i = do
+    s <- readChan c
+    case s of
+      Stop -> putMVar m i 
+      (Line _) -> loop (i + 1) 
+  
+
+main = do
+  [file] <- getArgs
+  c <- newChan
+  m <- newEmptyMVar
+  forkIO $ worker c m
+  master file c m
