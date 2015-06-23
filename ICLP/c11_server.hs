f50bbf66f8f19631189b42b542cea18a14733853
Vlad-Doru Ion <vlad.doru@gmail.com>
2015-06-23 15:38:59 +0300

    INcomplete c11.

--- ICLP/c11_server.hs
+++ ICLP/c11_server.hs
@@ -1,44 +1,65 @@
 import Control.Concurrent
+import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad
+import Data.Maybe
+import Data.List
 import Network
 import System.IO
 import System.IO.Error
 import Text.Read
 import Text.Printf
 
-handler :: IOError -> IO ()
-handler e = putStrLn $ "Whoops: " ++ (ioeGetErrorString e)
+errorHandling :: IOError -> IO ()
+errorHandling e = putStrLn "Closing client."
 
-talk :: Handle -> IO ()
-talk h = do
+type Factor = TVar Integer
+data Client = Client {handle::Handle chan::(TChan String)} deriving (Eq)
+
+newClient :: Handle -> Client
+newClient h = Client {handle = h, chan = newTChan}
+
+newFactor :: Integer -> IO Factor
+newFactor n = atomically $ do
+  f <- newTVar n
+  return f
+
+changeFactor :: Factor -> Integer -> IO ()
+changeFactor f n = do
+  atomically $ do
+    writeTVar f n
+  putStrLn $ "Changed the factor to: " ++ (show n)
+
+multiplyWithFactor :: Integer -> Factor -> IO Integer
+multiplyWithFactor x f = atomically $ do
+  n <- readTVar f
+  return (x * n)
+
+talk :: Client -> Factor -> IO ()
+talk c f = do
   hSetBuffering h LineBuffering
   loop where
   loop = do
+    let h = handle c
     l <- hGetLine h 
-    if 
-      l == "end"
-    then 
-      do
-        hPutStrLn h "Closing the connection with the server. Thanks for using us!"
-        hClose h
-    else
-      do
-        let nr = readMaybe l :: (Maybe Integer)
-        case nr of
-          Nothing -> hPrintf h "This line cannor be converted to an int: %s\n" l
-          Just x -> hPutStrLn h (show (2 * x))
-        loop
 
 port = 8888 :: Integer
 
+serveClient :: Socket -> Factor -> Clients -> IO ()
+serveClient sock factor clients = do 
+  (handle, host, port) <- accept sock
+  let client = newClient handle
+  addClient client clients
+  printf "Accepted connection from %s:%s\n" host (show port)
+  forkIO $ (talk client factor `finally` removeClient client clients) `catch` errorHandling 
+  return ()
+
 main = do 
   sock <- listenOn(PortNumber(fromIntegral port))
+  factor <- newFactor 2
+  clients <- newEmptyClientsList
   printf "Listening on port %d\n" port
-  forever $ do
-    (handle, host, port) <- accept sock
-    printf "Accepted connection from %s:%s\n" host (show port)
-    forkIO $ talk handle `catch` handler
+  forever $ serveClient sock factor clients
 
 
   
