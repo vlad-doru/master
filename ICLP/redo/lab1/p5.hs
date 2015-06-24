a9cdf4f7fc76d51e8c1a9b1452085bf4256e16e2
Vlad-Doru Ion <vlad.doru@gmail.com>
2015-06-24 14:22:47 +0300

    p5 l1

---
+++ ICLP/redo/lab1/p5.hs
@@ -0,0 +1,8 @@
+map2 :: (a -> b -> c) -> [a] -> [b] -> [c]
+map2 f x y 
+  | length x == length y = map (\(m, n) -> f m n) (zip x y)
+  | otherwise = error "Lungimi diferite ale lsitelor."
+
+main = do
+  putStrLn . show $ map2 (+) [1, 2, 3] [3, 0, -1]
+  putStrLn . show $ map2 (+) [1, 2, 3] [3, 0]
