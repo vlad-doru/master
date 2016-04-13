77123382ead79777b7342106c0c00d18cd49d392
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-04-13 13:01:29 +0300

    Solved the problem sequentially.

--- PPC-Adv/indian_grammar.py
+++ PPC-Adv/indian_grammar.py
@@ -1,6 +1,12 @@
 import sys
+import collections
 
 LAMBDA = "\\"
+DEBUG = False
+
+def debug(*args):
+    if DEBUG:
+        print(*args)
 
 class IndianGrammar(object):
 
@@ -28,7 +34,7 @@ class IndianGrammar(object):
                 exit(1)
         print("Terminale:", ', '.join(self.T))
         ### PRODUCTIONS
-        self.P = []
+        self.P = collections.defaultdict(list)
         productions = lines[2:-1]
         print("Productii:")
         for p in productions:
@@ -41,7 +47,7 @@ class IndianGrammar(object):
                 for x in out:
                     if not ((x == LAMBDA) or (x in self.N) or (x in self.T)):
                         raise Exception()
-                self.P.append((n, out))
+                self.P[n].append(out)
                 print("\t{0} -> {1}".format(n, ' '.join(out)))
             except:
                 print("Productiile trebuie sa fie de forma N -> ...")
@@ -52,6 +58,48 @@ class IndianGrammar(object):
             print("Simbolul de start trebuie sa fie parte din neterminale")
             exit(1)
         print("Simbolul de start: ", self.S)
+    
+    def vocabulary(self, max_depth):
+        return self._voc([self.S], max_depth)
+
+    def _voc(self, left, max_depth, tabs = ""):
+        if left == LAMBDA:
+            return set("")
+        # See how many non terminals we have in the left side.
+        left_n = set()
+        for x in left:
+            if x in self.N:
+                left_n.add(x)
+        # If we have zero terminals then we have a word.
+        if len(left_n) == 0:
+            word = ""
+            for x in left:
+                if x == LAMBDA:
+                    continue
+                word = word + x
+            return set([word])
+        # If we canoot do any more derivations we stop.
+        if max_depth == 0:
+            return set()
+        words = set()
+        # For each non terminal try to derive it's productions.
+        debug(tabs, "LEFT", left)
+        for n in left_n:
+            for prod in self.P[n]:
+                debug(tabs, "PROD", n, prod)
+                right = []
+                for x in left:
+                    if x == n:
+                        right = right + prod 
+                    else:
+                        right.append(x)
+                debug(tabs, "RIGHT", right)
+                aux_voc = self._voc(right, max_depth - 1, tabs = tabs + "\t")
+                debug(tabs, "VOC", aux_voc)
+                for x in aux_voc:
+                    words.add(x)
+        return words
+        
         
 
 def main():
@@ -59,6 +107,21 @@ def main():
         print("Dati ca argument fisierul ce descrie gramatica.")
     file_path = sys.argv[1]
     grammar = IndianGrammar(file_path)
+    while True:
+        aux = input("Numarul maxim de derivari:")   
+        if aux.strip() == "":
+            break
+        max_depth = 0
+        try:
+            max_depth = int(aux)
+            if max_depth <= 0 :
+                raise Exception()
+        except:
+            print("Introduceti un numar valid (> 0)")
+            continue
+        print("Calculam multimea cuvintelor obtinute prin maxim {0} derivari.".format(max_depth))
+        v = grammar.vocabulary(max_depth)
+        print("Cuvintele obtinute:", sorted(v))
     
 
 if __name__ == '__main__':
