d83cba82bce078c99177ce36c6bd371bf64f25d2
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-04-13 14:19:29 +0300

    We can use threads now.:

--- PPC-Adv/indian_grammar.py
+++ PPC-Adv/indian_grammar.py
@@ -2,6 +2,7 @@ import sys
 import collections
 import time
 import concurrent.futures
+import multiprocessing
 
 LAMBDA = "\\"
 DEBUG = False
@@ -10,9 +11,9 @@ def debug(*args):
     if DEBUG:
         print(*args)
 
-class IndianGrammar(object):
+class Grammar(object):
 
-    """Clasa pentru gramatica paralela indiana"""
+    """Clasa pentru gramatica"""
 
     def __init__(self, file_path):
         """Constructorul clasei fiind data calea unui fisier.
@@ -60,56 +61,64 @@ class IndianGrammar(object):
             print("Simbolul de start trebuie sa fie parte din neterminale")
             exit(1)
         print("Simbolul de start: ", self.S)
-    
-    def vocabulary(self, max_depth):
-        return self._voc([self.S], max_depth)
 
-    def _voc(self, left, max_depth, tabs = ""):
-        if left == LAMBDA:
-            return set("")
-        # See how many non terminals we have in the left side.
-        left_n = set()
+    
+def indian_derivation(grammar, max_depth):
+    return _voc(grammar, [grammar.S], max_depth)
+
+def _voc(grammar, left, max_depth, tabs = ""):
+    if left == LAMBDA:
+        return set("")
+    # See how many non terminals we have in the left side.
+    left_n = set()
+    for x in left:
+        if x in grammar.N:
+            left_n.add(x)
+    # If we have zero terminals then we have a word.
+    if len(left_n) == 0:
+        word = ""
         for x in left:
-            if x in self.N:
-                left_n.add(x)
-        # If we have zero terminals then we have a word.
-        if len(left_n) == 0:
-            word = ""
-            for x in left:
-                if x == LAMBDA:
-                    continue
-                word = word + x
-            return set([word])
-        # If we canoot do any more derivations we stop.
-        if max_depth == 0:
-            return set()
-        words = set()
-        # For each non terminal try to derive it's productions.
-        debug(tabs, "LEFT", left)
-        for ws in map(lambda n: self._expand(left, n, max_depth, tabs), left_n):
-            words = words.union(ws)
-        return words
-        
-    def _expand(self, left, n, max_depth, tabs):
-        voc = set()
-        for prod in self.P[n]:
-            debug(tabs, "PROD", n, prod)
-            right = []
-            for x in left:
-                if x == n:
-                    right = right + prod 
-                else:
-                    right.append(x)
-            aux_voc = self._voc(right, max_depth - 1, tabs = tabs + "\t")
-            voc = voc.union(aux_voc)
-        debug(tabs, "VOC", voc)
-        return voc
+            if x == LAMBDA:
+                continue
+            word = word + x
+        return set([word])
+    # If we canoot do any more derivations we stop.
+    if max_depth == 0:
+        return set()
+    words = set()
+    # For each non terminal try to derive it's productions.
+    debug(tabs, "LEFT", left)
+    args = [(grammar, left, n, max_depth, tabs) for n in left_n]
+    for ws in parallel(_expand, args):
+        words = words.union(ws)
+    return words
+    
+def _expand(args):
+    grammar, left, n, max_depth, tabs = args
+    voc = set()
+    for prod in grammar.P[n]:
+        debug(tabs, "PROD", n, prod)
+        right = []
+        for x in left:
+            if x == n:
+                right = right + prod 
+            else:
+                right.append(x)
+        aux_voc = _voc(grammar, right, max_depth - 1, tabs = tabs + "\t")
+        voc = voc.union(aux_voc)
+    debug(tabs, "VOC", voc)
+    return voc
+
+POOL = concurrent.futures.ThreadPoolExecutor()
+
+def parallel(f, l):
+    return POOL.map(f, l)
 
 def main():
     if len(sys.argv) != 2:
         print("Dati ca argument fisierul ce descrie gramatica.")
     file_path = sys.argv[1]
-    grammar = IndianGrammar(file_path)
+    grammar = Grammar(file_path)
     while True:
         aux = input("Numarul maxim de derivari:")   
         if aux.strip() == "":
@@ -124,9 +133,9 @@ def main():
             continue
         print("Calculam multimea cuvintelor obtinute prin maxim {0} derivari.".format(max_depth))
         start = time.time()
-        v = grammar.vocabulary(max_depth)
+        v = indian_derivation(grammar, max_depth)
         end = time.time()
-        print("Cuvintele obtinute:", sorted(v))
+        # print("Cuvintele obtinute:", sorted(v))
         print('Calculul a durat {0} s'.format(round((end - start), 3)))
     
 
