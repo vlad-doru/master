051f0e966f578498cf2db96fd76e7e776547b63c
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-04-15 00:49:03 +0300

    Setup the C++ program.

--- PPC-Adv/cpp/generate_input.py
+++ PPC-Adv/cpp/generate_input.py
@@ -1,10 +1,11 @@
 import numpy.random as rand
 
 def main():
-    for size, name in [(1000, '1K'), (10000, '10K'), (100000, '100K'), (1000000, '1M'), (10000000, '10M')]:
+    for size, name in [(1000000, '1M'), (10000000, '10M'), (50000000, '50M')]:
         print(size, name)
         with open('numbers_{0}.in'.format(name), 'w') as f:
-            nums = [str(rand.randint(0, 1 << 31)) for x in range(size)]
+            nums = [str(rand.randint(0, 2000000000)) for x in range(size)]
+            print(str(size) + '\n', file = f)
             print(' '.join(nums), file = f)
 if __name__ == '__main__':
     main()
--- PPC-Adv/cpp/sample_sort.cpp
+++ PPC-Adv/cpp/sample_sort.cpp
@@ -1,13 +1,50 @@
 #include <iostream>
+#include <ctime>
+#include <algorithm>
+#include <fstream>
+#include <vector>
 
 using namespace std;
+
+vector<int> read_input(string file_name)
+{
+	cout << "Start reading data.\n";
+	ifstream in(file_name);
+	int n;
+	in >> n;
+	vector<int> numbers;
+	for (int i = 0; i < n; ++i)
+	{
+		int x;	
+		in >> x;
+		numbers.push_back(x);
+	}
+	cout << "Reading data finished.\n";
+	return move(numbers);
+}
+
+void parallel_sort(vector<int>& numbers) {
+	sort(numbers.begin(), numbers.end());
+}
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 	{
-		cout << "Va rugam dati ca argument calea fisierului de intrare";
+		cout << "Va rugam dati ca argument calea fisierului de intrare\n";
 		return 1;
+	}
+	string file = string(argv[1]);
+	auto numbers = read_input(file);
+	auto start = clock();
+	parallel_sort(numbers);
+	cout << "Sorting time: " << (clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms\n";
+	// Check.
+	for (int i = 1; i < numbers.size(); ++i) {
+		if (numbers[i-1] > numbers[i]) {
+			cout << "Sortare gresita " << i;
+			return 1;
+		}
 	}
 	return 0;
 }
