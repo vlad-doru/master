51686ce9a77b22ac1cc782ba76d69766184fe30a
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-04-17 19:42:22 +0300

    Implemented cross validation.

--- TM/lib/classify.py
+++ TM/lib/classify.py
@@ -1,19 +1,23 @@
 import pickle
+import sys
+import random
 
 from nltk.util import bigrams
 from collections import defaultdict
 from nltk.sentiment.util import extract_unigram_feats, extract_bigram_feats
+from sklearn import cross_validation
 
-def add_features(train_data, sentim_analyzer, min_freq = 10):
+def add_features(data, sentim_analyzer, min_freq = 10):
     # Unigrams as features.
-    all_words = sentim_analyzer.all_words(train_data, labeled=True)
+    all_words = sentim_analyzer.all_words(data, labeled=True)
     unigram_feats = sentim_analyzer.unigram_word_feats(all_words, min_freq = min_freq)
     sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams = unigram_feats)
     print("Unigram features", len(unigram_feats))
+    sys.stdout.flush()
     # Bigrams for features.
     bigram_feats = []
     bigram_freq = defaultdict(int)
-    for tokens, _ in train_data:
+    for tokens, _ in data:
         for b in bigrams(tokens):
             bigram_freq[b] += 1
     for bigram, freq in bigram_freq.items():
@@ -22,26 +26,39 @@ def add_features(train_data, sentim_analyzer, min_freq = 10):
         else:
             bigram_feats.append(bigram)
     print("Bigram features: ", len(bigram_feats))
+    sys.stdout.flush()
     sentim_analyzer.add_feat_extractor(extract_bigram_feats, bigrams = bigram_feats)
 
-def extract_features(training_data, testing_data, sentim_analyzer):
-    return sentim_analyzer.apply_features(training_data), sentim_analyzer.apply_features(testing_data)
+def extract_features(data, sentim_analyzer):
+    return sentim_analyzer.apply_features(data)
 
-def train_model(training_data, testing_data, sentim_analyzer, trainer):
+def train_model(input_data, sentim_analyzer, trainer, sample_size = None):
+    if sample_size == None:
+        sample_size = len(input_data)
     if "name" in trainer:
         print("Training: {0}".format(trainer["name"]))
-    classifier = sentim_analyzer.train(trainer["train"], training_data)
-    # Evaluate the test set. 
-    evaluation = sentim_analyzer.evaluate(testing_data)
-    # Add to the models list.
-    # Save the classifier to a file.
-    if "model_file" in trainer:
-        model_file="./models/{}.pickle".format(trainer["model_file"])
-        print("Saving the model to file: {0}".format(model_file))
-        f = open(model_file, "wb")
-        pickle.dump({
-            "classifier": classifier,
-            "evaluation": evaluation,
-            "trainer": trainer,
-        }, f, -1)
-    return (classifier, evaluation, trainer)
+        sys.stdout.flush()
+    data = random.sample(input_data, sample_size)
+    print("Sampled", len(data), "elements")
+    sys.stdout.flush()
+    cv = cross_validation.KFold(len(data), n_folds=10, shuffle = True)
+    evaluations = []
+    fold = 0
+    for train_index, test_index in cv:
+        train_set = set(train_index)
+        test_set = set(test_index)
+        fold+=1
+        print("\tTraining fold", fold)
+        sys.stdout.flush()
+
+        training_data = [x for i, x in enumerate(data) if i in train_set]
+        testing_data = [x for i, x in enumerate(data) if i in test_set]
+
+        training_features = extract_features(training_data, sentim_analyzer)
+        testing_features = extract_features(testing_data, sentim_analyzer)
+
+        classifier = sentim_analyzer.train(trainer["train"], training_features)
+        print("\tEvaluating fold", fold)
+        sys.stdout.flush()
+        evaluations.append(sentim_analyzer.evaluate(testing_features))
+    return evaluations
--- TM/lib/processing.py
+++ TM/lib/processing.py
@@ -6,27 +6,25 @@ from nltk.tokenize.casual import TweetTokenizer
 from nltk.sentiment.util import mark_negation 
 from nltk.corpus import stopwords
 
-def process_data(df, sample_size = 1.0, split_size = 0.9):
-    df = df.head(n = int(len(df) * sample_size))
-    mask = np.random.rand(len(df)) < split_size
-    training_data = tokenize_tweets(df[mask])
-    testing_data = tokenize_tweets(df[~mask])
+def process_data(df, negation = True):
+    data = tokenize_tweets(df, negation)
     # Log info
     print("Using a total of {0} tweets".format(len(df)))
-    print("Training sample size: {0}".format(len(training_data)))
-    print("Testing sample size: {0}".format(len(testing_data)))
-    return training_data, testing_data
+    return data
 
-def process_tweet(tweet):
-    # Use a tweet tokenizer form the nltk package.
-    tokenizer = TweetTokenizer()
-    tokens = tokenizer.tokenize(tweet)
-    filtered = [w for w in tokens if w not in stopwords.words('english')]
-    return mark_negation(filtered)
-
-def tokenize_tweets(data):
+def tokenize_tweets(data, negation):
     # Get the lowercase of the text.
     extracted = list(zip(data['text'].str.lower(), data['airline_sentiment']))
     # Use mark negation to capture better sentiment.
-    tokenized = [(process_tweet(text), label) for text, label in extracted] 
+    tokenized = [(process_tweet(text, negation), label) for text, label in extracted] 
     return tokenized
+
+def process_tweet(tweet, negation):
+    # Use a tweet tokenizer form the nltk package.
+    tokenizer = TweetTokenizer()
+    tokens = tokenizer.tokenize(tweet)
+    if negation:
+        return mark_negation(tokens)
+    else:
+        return tokens
+
