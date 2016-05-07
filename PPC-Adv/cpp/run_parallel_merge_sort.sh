09574cfe25ec5245205aaeeedca0c1d9eab8e13c
Vlad-Doru Ion <vlad.doru@gmail.com>
2016-05-07 15:28:28 +0300

    Added script for compile & run.

---
+++ PPC-Adv/cpp/run_parallel_merge_sort.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11  benchmark_parallel_merge_sort.cpp -I /usr/local/include -ltbb -o benchmark_parallel_merge_sort && ./benchmark_parallel_merge_sort
---
+++ PPC-Adv/cpp/run_sample_sort.sh
@@ -0,0 +1,1 @@
+g++ -std=c++11  benchmark_sample_sort.cpp -I /usr/local/include -ltbb -o benchmark_sample_sort && ./benchmark_sample_sort
